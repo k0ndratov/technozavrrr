f35f654188960c532b96a0cd0fa74c0d4371ac99
kondratov <peckondratov@gmail.com>
2022-08-23 17:45:43 +0300

    add filtering algorithm

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,73 @@
 <template>
-<div>
-  <section class="catalog">
-    <ProductList :products="products"/>
-    <BasePagination v-model="page" :count="productsCount" :per-page="productsPerPage"/>
-</section>
-</div>
+  <div>
+    <main class="content container">
+      <div class="content__top content__top--catalog">
+        <h1 class="content__title">
+          Каталог
+        </h1>
+        <span class="content__info">
+          152 товара
+        </span>
+      </div>
+
+      <div class="content__catalog">
+        <ProductFilter/>
+        <section class="catalog">
+          <ProductList :products="products"/>
+          <BasePagination v-model="page" :count="productsCount" :per-page="productsPerPage"/>
+        </section>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 import products from './data/products';
 import ProductList from './components/ProductList.vue';
 import BasePagination from './components/BasePagination.vue';
+import ProductFilter from './components/ProductFilter.vue';
 
 export default {
   name: 'App',
   components: {
     ProductList,
     BasePagination,
+    ProductFilter,
   },
   data() {
     return {
+      filterFrom: 0,
+      filterTo: 0,
+      filterCategoryId: 0,
+
       page: 1,
       productsPerPage: 3,
     };
   },
   computed: {
+    filteredProducts() {
+      let fulteredProducts;
+
+      fulteredProducts = this.filterFrom
+        ? products.filter((p) => p.price >= this.filterFrom)
+        : products;
+
+      fulteredProducts = this.filterTo
+        ? fulteredProducts.filter((p) => p.price <= this.filterTo)
+        : fulteredProducts;
+
+      fulteredProducts = this.filterCategoryId
+        ? fulteredProducts.filter((p) => p.categoryId === this.filterCategoryId)
+        : fulteredProducts;
+
+      return fulteredProducts;
+    },
     productsCount() {
-      return products.length;
+      return this.filteredProducts.length;
     },
     products() {
       const offset = (this.page - 1) * this.productsPerPage;
-      return products.slice(offset, offset + this.productsPerPage);
+      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
     },
   },
 };
--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -1,39 +1,41 @@
 <template>
-  <ul class="catalog__pagination pagination">
-    <li class="pagination__item">
-      <a
-        class="pagination__link pagination__link--arrow"
-        aria-label="Предыдущая страница"
-        :class="{'pagination__link--disabled': page===1}"
-        href="#"
-        @click.prevent="paginate(page-1)">
-        <svg width="8" height="14" fill="currentColor">
-          <use xlink:href="#icon-arrow-left"></use>
-        </svg>
-      </a>
-    </li>
-    <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
-      <a
-        href="#"
-        class="pagination__link"
-        :class="{'pagination__link--current': pageNumber === page}"
-        @click.prevent="paginate(pageNumber)">
-        {{ pageNumber }}
-      </a>
-    </li>
-    <li class="pagination__item">
-      <a
-        class="pagination__link pagination__link--arrow"
-        href="#"
-        aria-label="Следующая страница"
-        :class="{'pagination__link--disabled': page===pages}"
-        @click.prevent="paginate(page+1)">
-        <svg width="8" height="14" fill="currentColor">
-          <use xlink:href="#icon-arrow-right"></use>
-        </svg>
-      </a>
-    </li>
-  </ul>
+  <div>
+    <ul class="catalog__pagination pagination">
+      <li class="pagination__item">
+        <a
+          class="pagination__link pagination__link--arrow"
+          aria-label="Предыдущая страница"
+          :class="{'pagination__link--disabled': page===1}"
+          href="#"
+          @click.prevent="paginate(page-1)">
+          <svg width="8" height="14" fill="currentColor">
+            <use xlink:href="#icon-arrow-left"></use>
+          </svg>
+        </a>
+      </li>
+      <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
+        <a
+          href="#"
+          class="pagination__link"
+          :class="{'pagination__link--current': pageNumber === page}"
+          @click.prevent="paginate(pageNumber)">
+          {{ pageNumber }}
+        </a>
+      </li>
+      <li class="pagination__item">
+        <a
+          class="pagination__link pagination__link--arrow"
+          href="#"
+          aria-label="Следующая страница"
+          :class="{'pagination__link--disabled': page===pages}"
+          @click.prevent="paginate(page+1)">
+          <svg width="8" height="14" fill="currentColor">
+            <use xlink:href="#icon-arrow-right"></use>
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
